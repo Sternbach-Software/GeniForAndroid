3ce5305d365aea88132835711bdfd65740fba5c2
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-22 05:43:50 -0500

    Translate Status.java to kotlin

--- app/src/main/java/app/familygem/constant/Status.kt
+++ app/src/main/java/app/familygem/constant/Status.kt
@@ -1,44 +1,41 @@
-package app.familygem.constant;
+package app.familygem.constant
 
-import org.folg.gedcom.model.EventFact;
-import org.folg.gedcom.model.Family;
+import org.folg.gedcom.model.Family
+import org.folg.gedcom.model.EventFact
+
 /**
  * Family situation
- * */
-public enum Status {
+ */
+enum class Status {
+    /**
+     * Generic relationship
+     * */
+    NONE,
+    MARRIED,
+    DIVORCED,
+    SEPARATED;
 
-	NONE, // Generic relationship
-	MARRIED, DIVORCED, SEPARATED;
-
-	/**
-	 * Finds the status of [family]
-	 * */
-	public static Status getStatus(Family family) {
-		Status status = NONE;
-		if( family != null ) {
-			for( EventFact event : family.getEventsFacts() ) {
-				String tag = event.getTag();
-				switch( tag ) {
-					case "MARR":
-						String type = event.getType();
-						if( type == null || type.isEmpty() || type.equals("marriage")
-								|| type.equals("civil") || type.equals("religious") || type.equals("common law") )
-							status = MARRIED;
-						else
-							status = NONE;
-						break;
-					case "MARB":
-					case "MARC":
-					case "MARL":
-					case "MARS":
-						status = MARRIED;
-						break;
-					case "DIV":
-						status = status == MARRIED ? DIVORCED : SEPARATED;
-						break;
-				}
-			}
-		}
-		return status;
-	}
+    companion object {
+        /**
+         * Finds the status of [family]
+         */
+        @JvmStatic
+        fun getStatus(family: Family?): Status {
+            var status = NONE
+            if (family != null) {
+                for (event in family.eventsFacts) {
+                    when (event.tag) {
+                        "MARR" -> {
+                            val type = event.type
+                            status =
+                                if (type == null || type.isEmpty() || type == "marriage" || type == "civil" || type == "religious" || type == "common law") MARRIED else NONE
+                        }
+                        "MARB", "MARC", "MARL", "MARS" -> status = MARRIED
+                        "DIV" -> status = if (status == MARRIED) DIVORCED else SEPARATED
+                    }
+                }
+            }
+            return status
+        }
+    }
 }
