97228cbcd8fdb546581c38e811aabfbc344e71a9
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-22 05:36:14 -0500

    Translate Choice.java to kotlin

--- app/src/main/java/app/familygem/constant/Choice.kt
+++ app/src/main/java/app/familygem/constant/Choice.kt
@@ -1,9 +1,9 @@
-package app.familygem.constant;
+package app.familygem.constant
 
-public class Choice {
-	public static final String PERSON = "choosePerson";
-	public static final String MEDIA = "chooseMedia";
-	public static final String NOTE = "chooseNote";
-	public static final String SOURCE = "chooseSource";
-	public static final String REPOSITORY = "chooseRepo";
+object Choice {
+    const val PERSON = "choosePerson"
+    const val MEDIA = "chooseMedia"
+    const val NOTE = "chooseNote"
+    const val SOURCE = "chooseSource"
+    const val REPOSITORY = "chooseRepo"
 }
