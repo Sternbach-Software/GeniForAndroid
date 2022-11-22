17971587d09bed3f644c845c1a1d37b359ad30d0
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-22 17:37:24 -0500

    Translate NoteActivity.java to kotlin

--- app/src/main/java/app/familygem/detail/NoteActivity.kt
+++ app/src/main/java/app/familygem/detail/NoteActivity.kt
@@ -1,44 +1,41 @@
-package app.familygem.detail;
+package app.familygem.detail
 
-import android.app.Activity;
-import org.folg.gedcom.model.Note;
-import app.familygem.DetailActivity;
-import app.familygem.Global;
-import app.familygem.Memory;
-import app.familygem.R;
-import app.familygem.U;
-import app.familygem.visitor.NoteReferences;
+import app.familygem.visitor.NoteReferences
+import android.app.Activity
+import app.familygem.*
+import org.folg.gedcom.model.Note
 
-public class NoteActivity extends DetailActivity {
+class NoteActivity : DetailActivity() {
 
-	Note n;
+    lateinit var n: Note
 
-	@Override
-	public void format() {
-		n = (Note)cast(Note.class);
-		if( n.getId() == null ) {
-			setTitle(R.string.note);
-			placeSlug("NOTE");
-		} else {
-			setTitle(R.string.shared_note);
-			placeSlug("NOTE", n.getId());
-		}
-		place(getString(R.string.text), "Value", true, true);
-		place(getString(R.string.rin), "Rin", false, false);
-		placeExtensions(n);
-		U.placeSourceCitations(box, n);
-		U.placeChangeDate(box, n.getChange());
-		if( n.getId() != null ) {
-			NoteReferences noteRef = new NoteReferences(Global.gc, n.getId(), false);
-			if( noteRef.count > 0 )
-				U.putContainer(box, noteRef.founders.toArray(), R.string.shared_by);
-		} else if( ((Activity)box.getContext()).getIntent().getBooleanExtra("fromNotes", false) ) {
-			U.putContainer(box, Memory.firstObject(), R.string.written_in);
-		}
-	}
+    override fun format() {
+        n = cast(Note::class.java) as Note
+        if (n.id == null) {
+            setTitle(R.string.note)
+            placeSlug("NOTE")
+        } else {
+            setTitle(R.string.shared_note)
+            placeSlug("NOTE", n.id)
+        }
+        place(getString(R.string.text), "Value", true, true)
+        place(getString(R.string.rin), "Rin", false, false)
+        placeExtensions(n)
+        U.placeSourceCitations(box, n)
+        U.placeChangeDate(box, n.change)
+        if (n.id != null) {
+            val noteRef = NoteReferences(Global.gc, n.id, false)
+            if (noteRef.count > 0) U.putContainer(
+                box,
+                noteRef.founders.toTypedArray(),
+                R.string.shared_by
+            )
+        } else if ((box.context as Activity).intent.getBooleanExtra("fromNotes", false)) {
+            U.putContainer(box, Memory.firstObject(), R.string.written_in)
+        }
+    }
 
-	@Override
-	public void delete() {
-		U.updateChangeDate(U.deleteNote(n, null));
-	}
+    override fun delete() {
+        U.updateChangeDate(*U.deleteNote(n, null))
+    }
 }
