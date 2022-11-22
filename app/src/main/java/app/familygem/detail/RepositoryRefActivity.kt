ec7ae534b966042f6225ce6eb7a0063ee54257a5
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-22 17:40:49 -0500

    Translate RepositoryRefActivity.java to kotlin

--- app/src/main/java/app/familygem/detail/RepositoryRefActivity.kt
+++ app/src/main/java/app/familygem/detail/RepositoryRefActivity.kt
@@ -1,65 +1,67 @@
-package app.familygem.detail;
+package app.familygem.detail
 
-import android.content.Context;
-import android.content.Intent;
-import androidx.cardview.widget.CardView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import org.folg.gedcom.model.Repository;
-import org.folg.gedcom.model.RepositoryRef;
-import org.folg.gedcom.model.Source;
-import app.familygem.DetailActivity;
-import app.familygem.Memory;
-import app.familygem.R;
-import app.familygem.U;
-import static app.familygem.Global.gc;
+import org.folg.gedcom.model.RepositoryRef
+import app.familygem.detail.RepositoryRefActivity
+import android.widget.LinearLayout
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import android.content.Intent
+import android.view.View
+import app.familygem.*
+import app.familygem.detail.RepositoryActivity
+import org.folg.gedcom.model.Repository
+import org.folg.gedcom.model.Source
 
-public class RepositoryRefActivity extends DetailActivity {
+class RepositoryRefActivity : DetailActivity() {
+    
+    lateinit var r: RepositoryRef
+    
+    override fun format() {
+        placeSlug("REPO")
+        r = cast(RepositoryRef::class.java) as RepositoryRef
+        if (r.getRepository(Global.gc) != null) { // valid
+            setTitle(R.string.repository_citation)
+            val repositoryCard = putRepository(box, r.getRepository(Global.gc))
+            repositoryCard.setTag(
+                R.id.tag_object,
+                r.getRepository(Global.gc)
+            ) //for the context menu TODO still needed?
+            registerForContextMenu(repositoryCard)
+        } else if (r.ref != null) { // of a non-existent archive (perhaps deleted) //di un archivio inesistente (magari eliminato)
+            setTitle(R.string.inexistent_repository_citation)
+        } else { // without ref??
+            setTitle(R.string.repository_note)
+        }
+        place(getString(R.string.value), "Value", false, true)
+        place(getString(R.string.call_number), "CallNumber")
+        place(getString(R.string.media_type), "MediaType")
+        placeExtensions(r)
+        U.placeNotes(box, r, true)
+    }
 
-	RepositoryRef r;
+    override fun delete() {
+        // Delete the citation from the archive and update the date of the source that contained it
+        val container = Memory.getSecondToLastObject() as Source
+        container.repositoryRef = null
+        U.updateChangeDate(container)
+        Memory.setInstanceAndAllSubsequentToNull(r)
+    }
 
-	@Override
-	public void format() {
-		placeSlug("REPO");
-		r = (RepositoryRef)cast(RepositoryRef.class);
-		if( r.getRepository(gc) != null ) { // valid
-			setTitle(R.string.repository_citation);
-			View repositoryCard = putRepository(box, r.getRepository(gc));
-			repositoryCard.setTag(R.id.tag_object, r.getRepository(gc)); //for the context menu TODO still needed?
-			registerForContextMenu(repositoryCard);
-		} else if( r.getRef() != null ) { // of a non-existent archive (perhaps deleted) //di un archivio inesistente (magari eliminato)
-			setTitle(R.string.inexistent_repository_citation);
-		} else { // without ref??
-			setTitle(R.string.repository_note);
-		}
-		place(getString(R.string.value), "Value", false, true);
-		place(getString(R.string.call_number), "CallNumber");
-		place(getString(R.string.media_type), "MediaType");
-		placeExtensions(r);
-		U.placeNotes(box, r, true);
-	}
-
-	public static View putRepository(LinearLayout container, final Repository repo) {
-		final Context context = container.getContext();
-		View repositoryCard = LayoutInflater.from(context).inflate(R.layout.pezzo_fonte, container, false);
-		container.addView(repositoryCard);
-		((TextView)repositoryCard.findViewById(R.id.fonte_testo)).setText(repo.getName());
-		((CardView)repositoryCard).setCardBackgroundColor(context.getResources().getColor(R.color.repository));
-		repositoryCard.setOnClickListener(v -> {
-			Memory.setFirst(repo);
-			context.startActivity(new Intent(context, RepositoryActivity.class));
-		});
-		return repositoryCard;
-	}
-
-	@Override
-	public void delete() {
-		// Delete the citation from the archive and update the date of the source that contained it
-		Source container = (Source) Memory.getSecondToLastObject();
-		container.setRepositoryRef(null);
-		U.updateChangeDate(container);
-		Memory.setInstanceAndAllSubsequentToNull(r);
-	}
+    companion object {
+        @JvmStatic
+        fun putRepository(container: LinearLayout, repo: Repository): View {
+            val context = container.context
+            val repositoryCard =
+                LayoutInflater.from(context).inflate(R.layout.pezzo_fonte, container, false)
+            container.addView(repositoryCard)
+            (repositoryCard.findViewById<View>(R.id.fonte_testo) as TextView).text = repo.name
+            (repositoryCard as CardView).setCardBackgroundColor(context.resources.getColor(R.color.repository))
+            repositoryCard.setOnClickListener { v: View? ->
+                Memory.setFirst(repo)
+                context.startActivity(Intent(context, RepositoryActivity::class.java))
+            }
+            return repositoryCard
+        }
+    }
 }
