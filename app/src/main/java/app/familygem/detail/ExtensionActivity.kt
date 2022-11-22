1cc019415126dbbbe143c8548ed850b30e061ec2
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-22 05:57:07 -0500

    Translate ExtensionActivity.java to kotlin

--- app/src/main/java/app/familygem/detail/ExtensionActivity.kt
+++ app/src/main/java/app/familygem/detail/ExtensionActivity.kt
@@ -1,35 +1,40 @@
-package app.familygem.detail;
+package app.familygem.detail
 
-import org.folg.gedcom.model.GedcomTag;
-import app.familygem.DetailActivity;
-import app.familygem.Memory;
-import app.familygem.R;
-import app.familygem.U;
+import app.familygem.DetailActivity
+import app.familygem.Memory
+import app.familygem.R
+import app.familygem.U
+import org.folg.gedcom.model.GedcomTag
 
-public class ExtensionActivity extends DetailActivity {
+class ExtensionActivity : DetailActivity() {
+    lateinit var e: GedcomTag
+    override fun format() {
+        title = getString(R.string.extension)
+        e = cast(GedcomTag::class.java) as GedcomTag
+        placeSlug(e.tag)
+        place(getString(R.string.id), "Id", false, false)
+        place(getString(R.string.value), "Value", true, true)
+        place("Ref", "Ref", false, false)
+        place(
+            "ParentTagName",
+            "ParentTagName",
+            false,
+            false
+        ) // I did not understand if it is used or not //non ho capito se viene usato o no
+        for (child in e.children) {
+            placePiece(
+                child.tag,
+                U
+                    .traverseExtension(child, 0)
+                    .removeSuffix("\n"),
+                child,
+                true
+            )
+        }
+    }
 
-	GedcomTag e;
-
-	@Override
-	public void format() {
-		setTitle(getString(R.string.extension));
-		e = (GedcomTag)cast(GedcomTag.class);
-		placeSlug(e.getTag());
-		place(getString(R.string.id), "Id", false, false);
-		place(getString(R.string.value), "Value", true, true);
-		place("Ref", "Ref", false, false);
-		place("ParentTagName", "ParentTagName", false, false); // I did not understand if it is used or not //non ho capito se viene usato o no
-		for( GedcomTag child : e.getChildren() ) {
-			String text = U.traverseExtension(child, 0);
-			if( text.endsWith("\n") )
-				text = text.substring(0, text.length() - 1);
-			placePiece(child.getTag(), text, child, true);
-		}
-	}
-
-	@Override
-	public void delete() {
-		U.deleteExtension(e, Memory.getSecondToLastObject(), null);
-		U.updateChangeDate(Memory.firstObject());
-	}
+    override fun delete() {
+        U.deleteExtension(e, Memory.getSecondToLastObject(), null)
+        U.updateChangeDate(Memory.firstObject())
+    }
 }
