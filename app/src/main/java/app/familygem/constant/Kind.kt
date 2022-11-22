98b5fbe5268972d6c155c1ef3db622c6a1ad43bd
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-22 05:40:48 -0500

    Translate Kind.java to kotlin

--- app/src/main/java/app/familygem/constant/Kind.kt
+++ app/src/main/java/app/familygem/constant/Kind.kt
@@ -1,18 +1,20 @@
-package app.familygem.constant;
+package app.familygem.constant
+
 /**
  * GEDCOM date types
- * */
-public enum Kind {
-
-	EXACT(""),
-	APPROXIMATE("ABT"), CALCULATED("CAL"), ESTIMATED("EST"),
-	AFTER("AFT"), BEFORE("BEF"), BETWEEN_AND("BET"),
-	FROM("FROM"), TO("TO"), FROM_TO("FROM"),
-	PHRASE("("); // Todo "INT"
-
-	public String prefix;
-
-	Kind(String prefix) {
-		this.prefix = prefix;
-	}
+ */
+enum class Kind(  // Todo "INT"
+    val prefix: String
+) {
+    EXACT(""),
+    APPROXIMATE("ABT"),
+    CALCULATED("CAL"),
+    ESTIMATED("EST"),
+    AFTER("AFT"),
+    BEFORE("BEF"),
+    BETWEEN_AND("BET"),
+    FROM("FROM"),
+    TO("TO"),
+    FROM_TO("FROM"),
+    PHRASE("(")
 }
