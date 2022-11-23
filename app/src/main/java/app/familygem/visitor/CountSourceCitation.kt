56de84dc587c4a7a5d8c9837f4ac7b8d56ade25c
shmueldabomb441 <shmuelsternbach613@gmail.com>
2022-11-23 09:50:28 -0500

    Translate CountSourceCitation.java to kotlin

--- app/src/main/java/app/familygem/visitor/CountSourceCitation.kt
+++ app/src/main/java/app/familygem/visitor/CountSourceCitation.kt
@@ -1,64 +1,41 @@
-package app.familygem.visitor;
+package app.familygem.visitor
 
-import org.folg.gedcom.model.EventFact;
-import org.folg.gedcom.model.Family;
-import org.folg.gedcom.model.Name;
-import org.folg.gedcom.model.Note;
-import org.folg.gedcom.model.Person;
-import org.folg.gedcom.model.SourceCitation;
-import org.folg.gedcom.model.Visitor;
+import org.folg.gedcom.model.*
 
 /**
  * Counter of citations of a source
- * It would be used to replace U.quanteQuante (Source) in the Library
+ * It would be used to replace [U.countCitations] (Source) in the Library
  * is more accurate in counting, that is, nothing escapes him, but is four times slower.
  *
  * // Contatore delle citazioni di una fonte
  * // Servirebbe per sostituire U.quanteCitazioni(Source) in Biblioteca
  * // è più preciso nella conta cioè non gli sfugge nulla, ma è quattro volte più lento
- * */
-public class CountSourceCitation extends Visitor {
+ */
+class CountSourceCitation(var id: String) : Visitor() {
+    var count = 0
+    override fun visit(p: Person): Boolean {
+        for (c in p.sourceCitations) if (c.ref != null) // required because source-notes have no Ref at source
+            if (c.ref == id) count++
+        return true
+    }
 
-	public int count = 0;
-	String id;
+    override fun visit(f: Family): Boolean {
+        for (c in f.sourceCitations) if (c.ref != null) if (c.ref == id) count++
+        return true
+    }
 
-	CountSourceCitation(String id ){
-		this.id = id;
-	}
+    override fun visit(n: Name): Boolean {
+        for (c in n.sourceCitations) if (c.ref != null) if (c.ref == id) count++
+        return true
+    }
 
-	@Override
-	public boolean visit( Person p ) {
-		for( SourceCitation c : p.getSourceCitations() )
-			if( c.getRef() != null ) // required because source-notes have no Ref at source
-				if( c.getRef().equals(id) ) count++;
-		return true;
-	}
-	@Override
-	public boolean visit( Family f ) {
-		for( SourceCitation c : f.getSourceCitations() )
-			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) count++;
-		return true;
-	}
-	@Override
-	public boolean visit( Name n ) {
-		for( SourceCitation c : n.getSourceCitations() )
-			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) count++;
-		return true;
-	}
-	@Override
-	public boolean visit( EventFact e ) {
-		for( SourceCitation c : e.getSourceCitations() )
-			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) count++;
-		return true;
-	}
-	@Override
-	public boolean visit( Note n ) {
-		for( SourceCitation c : n.getSourceCitations() )
-			if( c.getRef() != null )
-				if( c.getRef().equals(id) ) count++;
-		return true;
-	}
+    override fun visit(e: EventFact): Boolean {
+        for (c in e.sourceCitations) if (c.ref != null) if (c.ref == id) count++
+        return true
+    }
+
+    override fun visit(n: Note): Boolean {
+        for (c in n.sourceCitations) if (c.ref != null) if (c.ref == id) count++
+        return true
+    }
 }
